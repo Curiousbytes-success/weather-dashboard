97f4093613d93840ae32e24c52e2de217c1c4930
Curiousbytes-success <nayaksunidhi642@gmail.com>
2026-08-23 22:58:22 +0530

    Remove portfolio folder from weather dashboard repo

--- portfolio/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="site-footer wrap">
-      <span>© 2026 Sunidhi Nayak</span>
-      <span>Damoh, Madhya Pradesh, India</span>
-    </footer>
-  );
-}
